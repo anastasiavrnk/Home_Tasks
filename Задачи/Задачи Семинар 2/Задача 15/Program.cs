35fa6e9e5396810e47b9cb3dc6a8bb4f4898743c
Anastasiia Varenik <anastasia.varenik@yandex-team.ru>
2022-12-22 23:06:13 +0300

    description of 15 task added

--- Задачи/Задачи Семинар 2/Задача 15/Program.cs
+++ Задачи/Задачи Семинар 2/Задача 15/Program.cs
@@ -1,4 +1,6 @@
-﻿int number = int.Parse(Console.ReadLine()!);
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+int number = int.Parse(Console.ReadLine()!);
 if (number >=1 && number <=5)
 {
     Console.WriteLine("Нет");
