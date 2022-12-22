98d0e2d9b73af01bef29e106c98db1f7e5fe5568
Anastasiia Varenik <anastasia.varenik@yandex-team.ru>
2022-12-22 22:54:59 +0300

    task 13 added

---
+++ Задачи/Задача 13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+int number = int.Parse(Console.ReadLine()!);
+number = Math.Abs(number);
+
+if (number > 99)
+{   
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+int result = number % 10;
+Console.WriteLine(result);}
+
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
