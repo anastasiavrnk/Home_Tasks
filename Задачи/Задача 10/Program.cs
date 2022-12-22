e2823ab6ee6f7217dc16600a26e3cbdc6dd83a3d
Anastasiia Varenik <anastasia.varenik@yandex-team.ru>
2022-12-22 22:44:43 +0300

    task 10 added

---
+++ Задачи/Задача 10/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+int GetSecondDigit(int number)
+{
+    int firstDigit = number / 100;
+    int lastDigit = number % 10;
+    int secondDigit = (number - firstDigit*100 - lastDigit)/10;
+    return secondDigit;
+}
+
+int number = int.Parse(Console.ReadLine()!);
+if (number > 99 && number < 1000)
+{
+    Console.WriteLine(GetSecondDigit(number));
+}
+
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
---
+++ Задачи/Задача 2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b)
+{
+    Console.WriteLine ($"Максимальное число {a}, минимальное число {b}");
+}
+
+else if (a < b)
+{
+    Console.WriteLine ($"Максимальное число {b}, минимальное число {a}");
+}
+
+else
+{
+    Console.WriteLine ($"Число {a} равно числу {b}");
+}
