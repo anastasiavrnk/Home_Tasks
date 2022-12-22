1fd86a79963d754e20c86663d5005b1cb4750291
Anastasiia Varenik <anastasia.varenik@yandex-team.ru>
2022-12-22 22:55:59 +0300

    task 15 added

---
+++ Задачи/Задача 15/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = int.Parse(Console.ReadLine()!);
+if (number >=1 && number <=5)
+{
+    Console.WriteLine("Нет");
+}
+else if (number==6 || number ==7)
+{
+    Console.WriteLine("Да");
+}
