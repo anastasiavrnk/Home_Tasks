3f00c1d46cb1ff3b1ce9a29fd94de397741bb355
Anastasiia Varenik <anastasia.varenik@yandex-team.ru>
2022-12-26 22:24:40 +0300

    task 19 added

---
+++ Задачи/Задачи Семинар 3/Задача 19/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишите программу, которая принимает 
+//на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void PrintResultOfPalindrome(int number)
+
+{
+   if (number/10000 == number%10)
+    {
+        if ((number/1000)%10 == (number%100)/10)
+        {
+            Console.WriteLine($"Число {number} является палиндромом");
+            return;
+        }
+    }
+    Console.WriteLine($"Число {number} не является палиндромом");
+}
+
+
+
+int number = int.Parse(Console.ReadLine()!);
+
+if (number > 9999 && number < 99999)
+{
+    Console.WriteLine(PrintResultOfPalindrome(number));
+}
+
+else
+{
+    Console.WriteLine($"Число {number} является палиндромом");
+}
+
+
+
