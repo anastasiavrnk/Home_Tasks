de01957df4fae11adca5841ffce9c921e010aa8b
Anastasiia Varenik <anastasia.varenik@yandex-team.ru>
2022-12-26 23:06:01 +0300

    task 21  added

--- Задачи/Задачи Семинар 3/Задача 19/Program.cs
+++ Задачи/Задачи Семинар 3/Задача 19/Program.cs
@@ -11,22 +11,21 @@ void PrintResultOfPalindrome(int number)
             Console.WriteLine($"Число {number} является палиндромом");
             return;
         }
+
     }
-    Console.WriteLine($"Число {number} не является палиндромом");
+   Console.WriteLine($"Число {number} не является палиндромом");
 }
-
-
 
 int number = int.Parse(Console.ReadLine()!);
 
 if (number > 9999 && number < 99999)
 {
-    Console.WriteLine(PrintResultOfPalindrome(number));
+    PrintResultOfPalindrome(number);
 }
 
 else
 {
-    Console.WriteLine($"Число {number} является палиндромом");
+    Console.WriteLine($"Число {number} не является палиндромом");
 }
 
 
---
+++ Задачи/Задачи Семинар 3/Задача 21/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает 
+//на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double GetDistanceBetweenDots(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double result = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+    return result;
+}
+
+int x1 = int.Parse(Console.ReadLine()!);
+int y1 = int.Parse(Console.ReadLine()!);
+int z1 = int.Parse(Console.ReadLine()!);
+
+int x2 = int.Parse(Console.ReadLine()!);
+int y2 = int.Parse(Console.ReadLine()!);
+int z2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Расстояние равно {GetDistanceBetweenDots(x1, y1, z1, x2, y2, z2)}");
+
+
