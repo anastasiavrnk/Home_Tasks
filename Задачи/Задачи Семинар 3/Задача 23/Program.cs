56871b5d3d41a9c5b7300f49f1eb644450d82d2d
Anastasiia Varenik <anastasia.varenik@yandex-team.ru>
2022-12-27 22:04:50 +0300

    task 23 added

---
+++ Задачи/Задачи Семинар 3/Задача 23/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int N = int.Parse(Console.ReadLine()!);
+int i = 1;
+
+if (N > 1)
+{
+    while (i <= N)
+    {
+        double cubeOfNumber = Math.Pow(i, 3);
+        i++;
+        Console.WriteLine(cubeOfNumber);
+    }
+    return;   
+}
+
+else
+{
+    Console.WriteLine($"Число должно быть больше 1");
+}
