716d527f15a3c037834c0f3c3dd17b6ff529e3be
Anastasiia Varenik <anastasia.varenik@yandex-team.ru>
2023-01-24 21:54:29 +0300

    task 41/43 added

---
+++ Задачи/Задачи Семинар 6/Задача 41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите количество задаваемых чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int [] CreateArray(int M)
+{
+    int[] array = new int[M];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i+1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int NumbersGreaterThanZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+         if (array[i] > 0)
+         {
+             count++;
+         }
+     }
+     return count;
+}
+
+int[] array = CreateArray(M);
+int qtyNumbers = NumbersGreaterThanZero(array);
+
+Console.WriteLine($"Количество чисел больше нуля равно {qtyNumbers}");
+
---
+++ Задачи/Задачи Семинар 6/Задача 43/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+void IntersectionOfLines(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2 && b1 == b2)
+    {
+       Console.WriteLine("Две прямые имеют бесконечное количество точек соприкосновения, так как координаты двух прямых полностью совпадают");
+    }
+    else if (k1 == k2)
+    {
+        Console.WriteLine("Прямые параллельны");
+    }    
+    else
+    {
+        double x = (-b2 + b1)/(-k1 + k2);
+        double y = k2 * x + b2;
+        Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+    }
+}
+double b1 = 2;
+double k1 = 5;
+double b2 = 4;
+double k2 = 9;
+IntersectionOfLines(b1, k1, b2, k2);
